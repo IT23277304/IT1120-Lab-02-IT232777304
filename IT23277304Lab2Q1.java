23b66ec1d80e495a24fdc88b3021d246dcd22857
IT23277304 <140397604+IT23277304@users.noreply.github.com>
2024-08-07 09:12:49 +0530

    Add files via upload

---
+++ IT23277304Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT23277304Lab2Q1{
+
+public static void main(String[] args){
+double perimeter=100;
+double length=2*perimeter/7;
+double width=(3.0/4.0)*length;
+System.out.println("width is "+width);
+ System.out.println("length is "+length);
+
+ 
+
+}
+}
